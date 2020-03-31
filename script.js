bd3e1a040b896d559326c6650b5d35d120934206
Your Name <jchawkins725@gmail.com>
2020-03-31 14:17:39 -0400

    clean up code

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-const text = document.getElementById('text');
-const author = document.getElementById('author');
-const button = document.getElementById('new-quote');
-const container = document.getElementById('quote-author');
+const text = document.getElementById("text");
+const author = document.getElementById("author");
 
-//first quote render
-fetch('https://quota.glitch.me/random')
-.then(response => response.json())
-.then(data => {
-  text.innerHTML = data.quoteText;
-  author.innerHTML = "- " + data.quoteAuthor + " -";
-  $("#quote-author").animate(
-    { opacity: 1 }, 1000,
-  );
+function quoteCall(delay) {
+  fetch("https://quota.glitch.me/random")
+    .then(response => response.json())
+    .then(data => {
+      setTimeout(function() {
+        $('#text').html(data.quoteText);
+        $('#author').html("- " + data.quoteAuthor + " -");
+      }, delay);
+      $("#quote-author").animate({ opacity: 0 }, delay, function() {
+        $(this).animate({ opacity: 1 }, 500);
+      });
+    });
+}
+
+$("#new-quote").click(function() {
+  quoteCall(500);
 });
 
-//quote render on click
-button.onclick = function change() {
-  fetch('https://quota.glitch.me/random')
-  .then(response => response.json())
-  .then(data => {
-    setTimeout (function () {
-      text.innerHTML = data.quoteText;
-      author.innerHTML = "- " + data.quoteAuthor + " -";}, 500);
-    $("#quote-author").animate(
-      { opacity: 0 }, 500,
-       function() {
-         $(this).animate({ opacity: 1}, 500);
-    });
-  })
-};
-  
-  
-  $("#tweet-quote").click(function(event){
-    event.preventDefault();
-    window.open("https://twitter.com/intent/tweet?hashtags=quotes&text=" + "''" + text.innerHTML + "'' " + author.innerHTML.substring(0, author.innerHTML.length - 1)
-  )});
+$(document).ready(function() {
+  quoteCall(0);
+});
+
+$("#tweet-quote").click(function(event) {
+  event.preventDefault();
+  window.open(
+    "https://twitter.com/intent/tweet?hashtags=quotes&text=" +
+      "''" +
+      text.innerHTML +
+      "'' " +
+      author.innerHTML.substring(0, author.innerHTML.length - 1)
+  );
+});
